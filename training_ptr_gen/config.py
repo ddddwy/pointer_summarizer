404fc58e476dc1efb4c18e4b802176c4837abb93
think <310445479@qq.com>
2019-02-22 14:07:37 -0500

    modify logging

--- training_ptr_gen/config.py
+++ training_ptr_gen/config.py
@@ -13,8 +13,8 @@ log_root = os.path.join(root_dir, "pointer_summarizer/log")
 hidden_dim= 256
 emb_dim= 128
 batch_size= 8
-max_enc_steps=20
-max_dec_steps=20
+max_enc_steps=400
+max_dec_steps=200
 beam_size=4
 min_dec_steps=5
 vocab_size=5000
@@ -30,7 +30,7 @@ is_coverage = False
 cov_loss_wt = 1.0
 
 eps = 1e-12
-max_iterations = 50000
+max_iterations = 100000
 
 use_gpu=True
 
