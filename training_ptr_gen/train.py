eac14414312961b73c33ca7c03bc1db306659aa2
think <310445479@qq.com>
2019-02-22 00:08:43 -0500

    replace with Model

--- training_ptr_gen/train.py
+++ training_ptr_gen/train.py
@@ -129,30 +129,26 @@ class Train(object):
         dec_batch, dec_padding_mask, max_dec_len, dec_lens_var, target_batch = \
             get_output_from_batch(batch, use_cuda)
             
-        self.model.encoder.eval()
-        self.model.decoder.eval()
-        self.model.reduce_state.eval()    
-        with torch.no_grad():
-            encoder_outputs, encoder_feature, encoder_hidden = self.model.encoder(enc_batch, enc_lens)
-            s_t_1 = self.model.reduce_state(encoder_hidden)
-    
-            step_losses = []
-            for di in range(min(max_dec_len, config.max_dec_steps)):
-                y_t_1 = dec_batch[:, di]  # Teacher forcing
-                final_dist, s_t_1, c_t_1,attn_dist, p_gen, next_coverage = self.model.decoder(y_t_1, s_t_1,
-                                                            encoder_outputs, encoder_feature, enc_padding_mask, c_t_1,
-                                                            extra_zeros, enc_batch_extend_vocab, coverage, di)
-                target = target_batch[:, di]
-                gold_probs = torch.gather(final_dist, 1, target.unsqueeze(1)).squeeze()
-                step_loss = -torch.log(gold_probs + config.eps)
-                if config.is_coverage:
-                    step_coverage_loss = torch.sum(torch.min(attn_dist, coverage), 1)
-                    step_loss = step_loss + config.cov_loss_wt * step_coverage_loss
-                    coverage = next_coverage
-    
-                step_mask = dec_padding_mask[:, di]
-                step_loss = step_loss * step_mask
-                step_losses.append(step_loss)
+        encoder_outputs, encoder_feature, encoder_hidden = self.model.encoder(enc_batch, enc_lens)
+        s_t_1 = self.model.reduce_state(encoder_hidden)
+
+        step_losses = []
+        for di in range(min(max_dec_len, config.max_dec_steps)):
+            y_t_1 = dec_batch[:, di]  # Teacher forcing
+            final_dist, s_t_1, c_t_1,attn_dist, p_gen, next_coverage = self.model.decoder(y_t_1, s_t_1,
+                                                        encoder_outputs, encoder_feature, enc_padding_mask, c_t_1,
+                                                        extra_zeros, enc_batch_extend_vocab, coverage, di)
+            target = target_batch[:, di]
+            gold_probs = torch.gather(final_dist, 1, target.unsqueeze(1)).squeeze()
+            step_loss = -torch.log(gold_probs + config.eps)
+            if config.is_coverage:
+                step_coverage_loss = torch.sum(torch.min(attn_dist, coverage), 1)
+                step_loss = step_loss + config.cov_loss_wt * step_coverage_loss
+                coverage = next_coverage
+
+            step_mask = dec_padding_mask[:, di]
+            step_loss = step_loss * step_mask
+            step_losses.append(step_loss)
 
         sum_step_losses = torch.sum(torch.stack(step_losses, 1), 1)
         batch_avg_loss = sum_step_losses / dec_lens_var
@@ -192,10 +188,6 @@ class Train(object):
         start = time.time()
         min_val_loss = np.inf
         while iter < n_iters:
-            self.model.encoder().train()
-            self.model.decoder().train()
-            self.model.reduce_state().train()
-            
             batch = self.batcher.next_batch()
             loss = self.train_one_batch(batch)
 
@@ -208,12 +200,16 @@ class Train(object):
                                                                            time.time() - start, loss, min_val_loss))
                 start = time.time()
             if iter % 1000 == 0:
+                if iter == 1000:
+                    model_file_path = self.save_model(running_avg_loss, iter)
                 self.summary_writer.flush()
+                self.model = Model(model_file_path, is_eval=True)
                 val_avg_loss = self.run_eval()
+                self.model = Model(model_file_path, is_eval=False)
                 if val_avg_loss < min_val_loss:
                     min_val_loss = val_avg_loss
-                    train_model_path = self.save_model(running_avg_loss, iter)
-                    print('Save best model at %s'%train_model_path)
+                    model_file_path = self.save_model(running_avg_loss, iter)
+                    print('Save best model at %s'%model_file_path)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Train script")
