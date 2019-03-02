be4b77ab5f54eefe0e5dbeae235970212987a699
think <310445479@qq.com>
2019-03-02 00:24:42 -0500

    add list

--- pharase_gen/batcher.py
+++ pharase_gen/batcher.py
@@ -215,7 +215,8 @@ class Batcher(object):
           raise Exception("single_pass mode is off but the example generator is out of data; error.")
 
 #      abstract_sentences = [sent.strip() for sent in data.abstract2sents(abstract)] # Use the <s> and </s> tags in abstract to get a list of sentences.
-      abstract_sentences = [abstract]
+      abstract_sentences = []
+      abstract_sentences.append(abstract)
       example = Example(article, abstract_sentences, self._vocab) # Process into an Example.
       self._example_queue.put(example) # place the Example in the example queue.
 
